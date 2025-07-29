7ca9798b1a909e5cc2bed150b043552528e1a00d
arundhati-hive <arundhati.madhusudan@hiveminds.in>
2025-07-29 13:17:38 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st #ui
 import pypdf #to read and extract from the pdf
 import csv,re,os #csv to store chat history, re to display a clean output, os for path
